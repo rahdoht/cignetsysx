755fee2ee46288fdf47678550079e5ffe917e041
rahdoht <rahdoht@tutanota.com>
2023-05-23 01:22:10 -0500

    use img-preview to move stamp around in realtime

--- src/components/Crop.jsx
+++ src/components/Crop.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useRef, createRef } from "react";
+import React, { useState, createRef } from "react";
 import Cropper, { ReactCropperElement } from "react-cropper";
 import "cropperjs/dist/cropper.css";
 
+const staticPhotoSrc =
+  "https://bafybeigvhgkcqqamlukxcmjodalpk2kuy5qzqtx6m4i6pvb7o3ammss3y4.ipfs.dweb.link/2836.jpg";
 const defaultSrc =
   "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
 
 export const Stamp = () => {
   const [image, setImage] = useState(defaultSrc);
-  const [cropData, setCropData] = useState("#");
+  const [cropData, setCropData] = useState("");
   const cropperRef = createRef(null);
 
   const onChange = (e) => {
@@ -37,10 +39,27 @@ export const Stamp = () => {
   };
 
   return (
-    <div>
-      <div style={{ width: "60%" }}>
-        <input type="file" onChange={onChange} />
-        <br />
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div className="image-container">
+        <img src={staticPhotoSrc} alt="static" />
+        <div
+          className="img-preview"
+          style={{
+            position: "absolute",
+            left: 360,
+            top: 460,
+            width: 210,
+            height: 200,
+            transform: "matrix(1.2, -0.215, -0.02, 1.5, 0, 0)",
+          }}
+        >
+          .
+        </div>
+      </div>
+      <div style={{ padding: "20px" }}>
+        <div style={{ marginBottom: "10px" }}>
+          <input type="file" onChange={onChange} />
+        </div>
         <Cropper
           ref={cropperRef}
           style={{ height: 400, width: "100%" }}
@@ -49,22 +68,15 @@ export const Stamp = () => {
           preview=".img-preview"
           src={image}
           viewMode={1}
-          crop={onCrop}
+          minCropBoxHeight={10}
+          minCropBoxWidth={10}
+          background={false}
           responsive={true}
           autoCropArea={1}
           checkOrientation={false}
           guides={false}
         />
       </div>
-      <div>
-          <h1>
-            <span>Crop</span>
-            <button style={{ float: "right" }} onClick={getCropData}>
-              Crop Image
-            </button>
-          </h1>
-          <img style={{ width: "20%" }} src={cropData} alt="cropped" />
-        </div>
     </div>
   );
 };
