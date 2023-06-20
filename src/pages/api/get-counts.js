fae1cc6657cd123bb5d5f2c8ad1aba9cfea9a0a5
rahdoht <rahdoht@tutanota.com>
2023-06-19 21:21:53 -0500

    hacks to ignore supabase

--- src/pages/api/get-counts.js
+++ src/pages/api/get-counts.js
@@ -1,11 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE
-);
+let supabase_exists =
+  process.env.NEXT_PUBLIC_SUPABASE_URL &&
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_ROLE;
+if (supabase_exists) {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE
+  );
+}
 
 export default async function handler(req, res) {
+  return res.status(200).json({ count: 0 });
   // get the number of rows in the gens table
   const { data, count, error } = await supabase
     .from("gens")
